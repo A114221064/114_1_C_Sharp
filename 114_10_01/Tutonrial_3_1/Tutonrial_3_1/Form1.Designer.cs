04e3075559153d75c884c77e770ee737ebbabcf1
A114221064 <A114221064@mail.shu.edu.tw>
2025-10-01 13:09:30 +0800

    10_1

---
+++ 114_10_01/Tutonrial_3_1/Tutonrial_3_1/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Tutonrial_3_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dayofweekTextBox = new System.Windows.Forms.TextBox();
+            this.monthTextBox = new System.Windows.Forms.TextBox();
+            this.dayOfMonthTextBox = new System.Windows.Forms.TextBox();
+            this.yearsTextBox = new System.Windows.Forms.TextBox();
+            this.dateOutput = new System.Windows.Forms.Label();
+            this.showdate = new System.Windows.Forms.Button();
+            this.delete = new System.Windows.Forms.Button();
+            this.leave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(102, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 48);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "星期：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(150, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 48);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "月：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(150, 221);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 48);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "日：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("新細明體", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(150, 295);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(116, 48);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "年：";
+            // 
+            // dayofweekTextBox
+            // 
+            this.dayofweekTextBox.Font = new System.Drawing.Font("新細明體", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.dayofweekTextBox.Location = new System.Drawing.Point(272, 56);
+            this.dayofweekTextBox.Name = "dayofweekTextBox";
+            this.dayofweekTextBox.Size = new System.Drawing.Size(291, 55);
+            this.dayofweekTextBox.TabIndex = 4;
+            // 
+            // monthTextBox
+            // 
+            this.monthTextBox.Font = new System.Drawing.Font("新細明體", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.monthTextBox.Location = new System.Drawing.Point(272, 134);
+            this.monthTextBox.Name = "monthTextBox";
+            this.monthTextBox.Size = new System.Drawing.Size(291, 55);
+            this.monthTextBox.TabIndex = 5;
+            // 
+            // dayOfMonthTextBox
+            // 
+            this.dayOfMonthTextBox.Font = new System.Drawing.Font("新細明體", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.dayOfMonthTextBox.Location = new System.Drawing.Point(272, 214);
+            this.dayOfMonthTextBox.Name = "dayOfMonthTextBox";
+            this.dayOfMonthTextBox.Size = new System.Drawing.Size(291, 55);
+            this.dayOfMonthTextBox.TabIndex = 6;
+            // 
+            // yearsTextBox
+            // 
+            this.yearsTextBox.Font = new System.Drawing.Font("新細明體", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.yearsTextBox.Location = new System.Drawing.Point(272, 288);
+            this.yearsTextBox.Name = "yearsTextBox";
+            this.yearsTextBox.Size = new System.Drawing.Size(291, 55);
+            this.yearsTextBox.TabIndex = 7;
+            // 
+            // dateOutput
+            // 
+            this.dateOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dateOutput.Font = new System.Drawing.Font("新細明體", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.dateOutput.Location = new System.Drawing.Point(92, 381);
+            this.dateOutput.Name = "dateOutput";
+            this.dateOutput.Size = new System.Drawing.Size(604, 76);
+            this.dateOutput.TabIndex = 8;
+            this.dateOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // showdate
+            // 
+            this.showdate.Font = new System.Drawing.Font("新細明體", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.showdate.Location = new System.Drawing.Point(92, 478);
+            this.showdate.Name = "showdate";
+            this.showdate.Size = new System.Drawing.Size(200, 51);
+            this.showdate.TabIndex = 9;
+            this.showdate.Text = "顯示日期";
+            this.showdate.UseVisualStyleBackColor = true;
+            this.showdate.Click += new System.EventHandler(this.showdate_Click);
+            // 
+            // delete
+            // 
+            this.delete.Font = new System.Drawing.Font("新細明體", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.delete.Location = new System.Drawing.Point(340, 478);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(140, 51);
+            this.delete.TabIndex = 10;
+            this.delete.Text = "清除";
+            this.delete.UseVisualStyleBackColor = true;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // leave
+            // 
+            this.leave.Font = new System.Drawing.Font("新細明體", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.leave.Location = new System.Drawing.Point(538, 478);
+            this.leave.Name = "leave";
+            this.leave.Size = new System.Drawing.Size(158, 51);
+            this.leave.TabIndex = 11;
+            this.leave.Text = "離開";
+            this.leave.UseVisualStyleBackColor = true;
+            this.leave.Click += new System.EventHandler(this.leave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 562);
+            this.Controls.Add(this.leave);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.showdate);
+            this.Controls.Add(this.dateOutput);
+            this.Controls.Add(this.yearsTextBox);
+            this.Controls.Add(this.dayOfMonthTextBox);
+            this.Controls.Add(this.monthTextBox);
+            this.Controls.Add(this.dayofweekTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox dayofweekTextBox;
+        private System.Windows.Forms.TextBox monthTextBox;
+        private System.Windows.Forms.TextBox dayOfMonthTextBox;
+        private System.Windows.Forms.TextBox yearsTextBox;
+        private System.Windows.Forms.Label dateOutput;
+        private System.Windows.Forms.Button showdate;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.Button leave;
+    }
+}
+
---
+++ 114_10_01/Tutonrial_3_1/Tutonrial_3_1/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tutonrial_3_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void leave_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void showdate_Click(object sender, EventArgs e)
+        {
+            string dayOFweek;
+            dayOFweek = dayofweekTextBox.Text;
+            string month = monthTextBox.Text;
+            string year = yearsTextBox.Text;
+            string dayOfMonth = dayOfMonthTextBox.Text;
+            string output = "中華民國"+year + "年" + month + "月" + dayOfMonth + "日" + ",星期"+dayOFweek;
+            dateOutput.Text = output;
+
+        }
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+            dayofweekTextBox.Text = "";
+            monthTextBox.Text = "";
+            yearsTextBox.Text = "";
+            dayOfMonthTextBox.Text = "";
+            dateOutput.Text = "";
+        }
+    }
+}
