aa360e1307e73fcf1b9cd4d3e97429e8926c11d3
A114221064 <A114221064@mail.shu.edu.tw>
2025-10-13 16:07:16 +0800

    10_13

---
+++ CHW3/3_1/3_1/Form1.Designer.cs
@@ -0,0 +1,398 @@
+﻿namespace _3_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.A = new System.Windows.Forms.Button();
+            this.smalla = new System.Windows.Forms.Button();
+            this.An = new System.Windows.Forms.Button();
+            this.smallAN = new System.Windows.Forms.Button();
+            this.bigthe = new System.Windows.Forms.Button();
+            this.the = new System.Windows.Forms.Button();
+            this.man = new System.Windows.Forms.Button();
+            this.woman = new System.Windows.Forms.Button();
+            this.dog = new System.Windows.Forms.Button();
+            this.cat = new System.Windows.Forms.Button();
+            this.car = new System.Windows.Forms.Button();
+            this.bicyle = new System.Windows.Forms.Button();
+            this.beautiful = new System.Windows.Forms.Button();
+            this.big = new System.Windows.Forms.Button();
+            this.small = new System.Windows.Forms.Button();
+            this.strange = new System.Windows.Forms.Button();
+            this.lookedat = new System.Windows.Forms.Button();
+            this.rode = new System.Windows.Forms.Button();
+            this.spoketo = new System.Windows.Forms.Button();
+            this.laughedat = new System.Windows.Forms.Button();
+            this.drove = new System.Windows.Forms.Button();
+            this.space = new System.Windows.Forms.Button();
+            this.period = new System.Windows.Forms.Button();
+            this.marvel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Clear = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // A
+            // 
+            this.A.Location = new System.Drawing.Point(104, 74);
+            this.A.Name = "A";
+            this.A.Size = new System.Drawing.Size(38, 26);
+            this.A.TabIndex = 0;
+            this.A.Text = "A";
+            this.A.UseVisualStyleBackColor = true;
+            this.A.Click += new System.EventHandler(this.A_Click);
+            // 
+            // smalla
+            // 
+            this.smalla.Location = new System.Drawing.Point(165, 74);
+            this.smalla.Name = "smalla";
+            this.smalla.Size = new System.Drawing.Size(38, 26);
+            this.smalla.TabIndex = 1;
+            this.smalla.Text = "a";
+            this.smalla.UseVisualStyleBackColor = true;
+            this.smalla.Click += new System.EventHandler(this.smalla_Click);
+            // 
+            // An
+            // 
+            this.An.Location = new System.Drawing.Point(222, 74);
+            this.An.Name = "An";
+            this.An.Size = new System.Drawing.Size(38, 26);
+            this.An.TabIndex = 2;
+            this.An.Text = "An";
+            this.An.UseVisualStyleBackColor = true;
+            this.An.Click += new System.EventHandler(this.An_Click);
+            // 
+            // smallAN
+            // 
+            this.smallAN.Location = new System.Drawing.Point(276, 74);
+            this.smallAN.Name = "smallAN";
+            this.smallAN.Size = new System.Drawing.Size(38, 26);
+            this.smallAN.TabIndex = 3;
+            this.smallAN.Text = "an";
+            this.smallAN.UseVisualStyleBackColor = true;
+            this.smallAN.Click += new System.EventHandler(this.smallAN_Click);
+            // 
+            // bigthe
+            // 
+            this.bigthe.Location = new System.Drawing.Point(330, 74);
+            this.bigthe.Name = "bigthe";
+            this.bigthe.Size = new System.Drawing.Size(38, 26);
+            this.bigthe.TabIndex = 4;
+            this.bigthe.Text = "The";
+            this.bigthe.UseVisualStyleBackColor = true;
+            this.bigthe.Click += new System.EventHandler(this.bigthe_Click);
+            // 
+            // the
+            // 
+            this.the.Location = new System.Drawing.Point(390, 74);
+            this.the.Name = "the";
+            this.the.Size = new System.Drawing.Size(38, 26);
+            this.the.TabIndex = 5;
+            this.the.Text = "the";
+            this.the.UseVisualStyleBackColor = true;
+            this.the.Click += new System.EventHandler(this.the_Click);
+            // 
+            // man
+            // 
+            this.man.Location = new System.Drawing.Point(90, 127);
+            this.man.Name = "man";
+            this.man.Size = new System.Drawing.Size(38, 26);
+            this.man.TabIndex = 6;
+            this.man.Text = "man";
+            this.man.UseVisualStyleBackColor = true;
+            this.man.Click += new System.EventHandler(this.man_Click);
+            // 
+            // woman
+            // 
+            this.woman.Location = new System.Drawing.Point(145, 127);
+            this.woman.Name = "woman";
+            this.woman.Size = new System.Drawing.Size(53, 26);
+            this.woman.TabIndex = 7;
+            this.woman.Text = "woman";
+            this.woman.UseVisualStyleBackColor = true;
+            this.woman.Click += new System.EventHandler(this.woman_Click);
+            // 
+            // dog
+            // 
+            this.dog.Location = new System.Drawing.Point(221, 127);
+            this.dog.Name = "dog";
+            this.dog.Size = new System.Drawing.Size(38, 26);
+            this.dog.TabIndex = 8;
+            this.dog.Text = "dog";
+            this.dog.UseVisualStyleBackColor = true;
+            this.dog.Click += new System.EventHandler(this.dog_Click);
+            // 
+            // cat
+            // 
+            this.cat.Location = new System.Drawing.Point(278, 127);
+            this.cat.Name = "cat";
+            this.cat.Size = new System.Drawing.Size(38, 26);
+            this.cat.TabIndex = 9;
+            this.cat.Text = "cat";
+            this.cat.UseVisualStyleBackColor = true;
+            this.cat.Click += new System.EventHandler(this.cat_Click);
+            // 
+            // car
+            // 
+            this.car.Location = new System.Drawing.Point(332, 127);
+            this.car.Name = "car";
+            this.car.Size = new System.Drawing.Size(38, 26);
+            this.car.TabIndex = 10;
+            this.car.Text = "car";
+            this.car.UseVisualStyleBackColor = true;
+            this.car.Click += new System.EventHandler(this.car_Click);
+            // 
+            // bicyle
+            // 
+            this.bicyle.Location = new System.Drawing.Point(386, 127);
+            this.bicyle.Name = "bicyle";
+            this.bicyle.Size = new System.Drawing.Size(53, 26);
+            this.bicyle.TabIndex = 11;
+            this.bicyle.Text = "bicyle";
+            this.bicyle.UseVisualStyleBackColor = true;
+            this.bicyle.Click += new System.EventHandler(this.bicyle_Click);
+            // 
+            // beautiful
+            // 
+            this.beautiful.Location = new System.Drawing.Point(104, 178);
+            this.beautiful.Name = "beautiful";
+            this.beautiful.Size = new System.Drawing.Size(67, 26);
+            this.beautiful.TabIndex = 12;
+            this.beautiful.Text = "beautiful";
+            this.beautiful.UseVisualStyleBackColor = true;
+            this.beautiful.Click += new System.EventHandler(this.beautiful_Click);
+            // 
+            // big
+            // 
+            this.big.Location = new System.Drawing.Point(194, 178);
+            this.big.Name = "big";
+            this.big.Size = new System.Drawing.Size(38, 26);
+            this.big.TabIndex = 13;
+            this.big.Text = "big";
+            this.big.UseVisualStyleBackColor = true;
+            this.big.Click += new System.EventHandler(this.big_Click);
+            // 
+            // small
+            // 
+            this.small.Location = new System.Drawing.Point(251, 178);
+            this.small.Name = "small";
+            this.small.Size = new System.Drawing.Size(62, 26);
+            this.small.TabIndex = 14;
+            this.small.Text = "small";
+            this.small.UseVisualStyleBackColor = true;
+            this.small.Click += new System.EventHandler(this.small_Click);
+            // 
+            // strange
+            // 
+            this.strange.Location = new System.Drawing.Point(332, 178);
+            this.strange.Name = "strange";
+            this.strange.Size = new System.Drawing.Size(82, 26);
+            this.strange.TabIndex = 15;
+            this.strange.Text = "strange";
+            this.strange.UseVisualStyleBackColor = true;
+            this.strange.Click += new System.EventHandler(this.strange_Click);
+            // 
+            // lookedat
+            // 
+            this.lookedat.Location = new System.Drawing.Point(46, 223);
+            this.lookedat.Name = "lookedat";
+            this.lookedat.Size = new System.Drawing.Size(82, 26);
+            this.lookedat.TabIndex = 16;
+            this.lookedat.Text = "looked at";
+            this.lookedat.UseVisualStyleBackColor = true;
+            this.lookedat.Click += new System.EventHandler(this.lookedat_Click);
+            // 
+            // rode
+            // 
+            this.rode.Location = new System.Drawing.Point(151, 223);
+            this.rode.Name = "rode";
+            this.rode.Size = new System.Drawing.Size(65, 26);
+            this.rode.TabIndex = 17;
+            this.rode.Text = "rode";
+            this.rode.UseVisualStyleBackColor = true;
+            this.rode.Click += new System.EventHandler(this.rode_Click);
+            // 
+            // spoketo
+            // 
+            this.spoketo.Location = new System.Drawing.Point(233, 223);
+            this.spoketo.Name = "spoketo";
+            this.spoketo.Size = new System.Drawing.Size(82, 26);
+            this.spoketo.TabIndex = 18;
+            this.spoketo.Text = "spoke to";
+            this.spoketo.UseVisualStyleBackColor = true;
+            this.spoketo.Click += new System.EventHandler(this.spoketo_Click);
+            // 
+            // laughedat
+            // 
+            this.laughedat.Location = new System.Drawing.Point(330, 223);
+            this.laughedat.Name = "laughedat";
+            this.laughedat.Size = new System.Drawing.Size(82, 26);
+            this.laughedat.TabIndex = 19;
+            this.laughedat.Text = "laughed at";
+            this.laughedat.UseVisualStyleBackColor = true;
+            this.laughedat.Click += new System.EventHandler(this.laughedat_Click);
+            // 
+            // drove
+            // 
+            this.drove.Location = new System.Drawing.Point(427, 223);
+            this.drove.Name = "drove";
+            this.drove.Size = new System.Drawing.Size(82, 26);
+            this.drove.TabIndex = 20;
+            this.drove.Text = "drove";
+            this.drove.UseVisualStyleBackColor = true;
+            this.drove.Click += new System.EventHandler(this.drove_Click);
+            // 
+            // space
+            // 
+            this.space.Location = new System.Drawing.Point(167, 272);
+            this.space.Name = "space";
+            this.space.Size = new System.Drawing.Size(82, 26);
+            this.space.TabIndex = 21;
+            this.space.Text = "(space)";
+            this.space.UseVisualStyleBackColor = true;
+            this.space.Click += new System.EventHandler(this.space_Click);
+            // 
+            // period
+            // 
+            this.period.Location = new System.Drawing.Point(268, 272);
+            this.period.Name = "period";
+            this.period.Size = new System.Drawing.Size(32, 26);
+            this.period.TabIndex = 22;
+            this.period.Text = ".";
+            this.period.UseVisualStyleBackColor = true;
+            this.period.Click += new System.EventHandler(this.period_Click);
+            // 
+            // marvel
+            // 
+            this.marvel.Location = new System.Drawing.Point(317, 272);
+            this.marvel.Name = "marvel";
+            this.marvel.Size = new System.Drawing.Size(32, 26);
+            this.marvel.TabIndex = 23;
+            this.marvel.Text = "!";
+            this.marvel.UseVisualStyleBackColor = true;
+            this.marvel.Click += new System.EventHandler(this.marvel_Click);
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(27, 319);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(521, 27);
+            this.label1.TabIndex = 24;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(145, 390);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(82, 26);
+            this.Clear.TabIndex = 25;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(317, 390);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(82, 26);
+            this.Exit.TabIndex = 26;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 450);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.marvel);
+            this.Controls.Add(this.period);
+            this.Controls.Add(this.space);
+            this.Controls.Add(this.drove);
+            this.Controls.Add(this.laughedat);
+            this.Controls.Add(this.spoketo);
+            this.Controls.Add(this.rode);
+            this.Controls.Add(this.lookedat);
+            this.Controls.Add(this.strange);
+            this.Controls.Add(this.small);
+            this.Controls.Add(this.big);
+            this.Controls.Add(this.beautiful);
+            this.Controls.Add(this.bicyle);
+            this.Controls.Add(this.car);
+            this.Controls.Add(this.cat);
+            this.Controls.Add(this.dog);
+            this.Controls.Add(this.woman);
+            this.Controls.Add(this.man);
+            this.Controls.Add(this.the);
+            this.Controls.Add(this.bigthe);
+            this.Controls.Add(this.smallAN);
+            this.Controls.Add(this.An);
+            this.Controls.Add(this.smalla);
+            this.Controls.Add(this.A);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button A;
+        private System.Windows.Forms.Button smalla;
+        private System.Windows.Forms.Button An;
+        private System.Windows.Forms.Button smallAN;
+        private System.Windows.Forms.Button bigthe;
+        private System.Windows.Forms.Button the;
+        private System.Windows.Forms.Button man;
+        private System.Windows.Forms.Button woman;
+        private System.Windows.Forms.Button dog;
+        private System.Windows.Forms.Button cat;
+        private System.Windows.Forms.Button car;
+        private System.Windows.Forms.Button bicyle;
+        private System.Windows.Forms.Button beautiful;
+        private System.Windows.Forms.Button big;
+        private System.Windows.Forms.Button small;
+        private System.Windows.Forms.Button strange;
+        private System.Windows.Forms.Button lookedat;
+        private System.Windows.Forms.Button rode;
+        private System.Windows.Forms.Button spoketo;
+        private System.Windows.Forms.Button laughedat;
+        private System.Windows.Forms.Button drove;
+        private System.Windows.Forms.Button space;
+        private System.Windows.Forms.Button period;
+        private System.Windows.Forms.Button marvel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button Exit;
+    }
+}
+
