41ca744d53046248be730f145022c86329b62e36
A114221064 <A114221064@mail.shu.edu.tw>
2025-10-15 12:54:33 +0800

    114_10_15

---
+++ 114_10_15/Tutorial 4-2/Payroll with Overtime/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Payroll_with_Overtime
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// 建構子：初始化表單元件
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent(); // 初始化所有 UI 元件
+        }
+
+        /// <summary>
+        /// 當使用者點擊「計算總薪資」按鈕時觸發的事件處理函式
+        /// </summary>
+        /// <param name="sender">事件來源物件</param>
+        /// <param name="e">事件參數</param>
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            // 此處可加入薪資計算邏輯
+            const decimal OTMULTIPLER = 1.5m; 
+            const decimal BASEHOURS = 40m; 
+
+            decimal hoursWorked;
+            decimal hourlyPayRate;
+            decimal grossPay;
+
+            try {
+                hoursWorked = decimal.Parse(hoursWorkedTextBox.Text);
+                hourlyPayRate = decimal.Parse(hourlyPayRateTextBox.Text);
+                if (hoursWorked <= BASEHOURS) {
+                    grossPay = hoursWorked * hourlyPayRate;
+                }
+                else {
+                    
+                    grossPay = (BASEHOURS * hourlyPayRate) + ((hoursWorked - BASEHOURS) * hourlyPayRate * OTMULTIPLER);
+                }
+                grossPayLabel.TextAlign = ContentAlignment.MiddleCenter;
+                grossPayLabel.Text = grossPay.ToString("C");
+
+            }
+            catch(Exception ex) { 
+                MessageBox.Show(ex.Message);
+
+            }
+        }
+
+        /// <summary>
+        /// 當使用者點擊「清除」按鈕時觸發的事件處理函式
+        /// </summary>
+        /// <param name="sender">事件來源物件</param>
+        /// <param name="e">事件參數</param>
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            // 清空工時輸入框
+            hoursWorkedTextBox.Text = "";
+            // 清空時薪輸入框
+            hourlyPayRateTextBox.Text = "";
+            // 清空總薪資顯示標籤
+            grossPayLabel.Text = "";
+
+            // 將游標焦點移回工時輸入框，方便使用者再次輸入
+            hoursWorkedTextBox.Focus();
+        }
+
+        /// <summary>
+        /// 當使用者點擊「離開」按鈕時觸發的事件處理函式
+        /// </summary>
+        /// <param name="sender">事件來源物件</param>
+        /// <param name="e">事件參數</param>
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // 關閉表單，結束程式
+            this.Close();
+        }
+    }
+}
---
+++ 114_10_15/Tutorial 4-3/Loan Qualifier/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Loan_Qualifier
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// 建構函式：初始化表單元件
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 當「資格審查」按鈕被點擊時執行的事件處理函式
+        /// 目前尚未實作審查邏輯
+        /// </summary>
+        /// <param name="sender">事件來源物件</param>
+        /// <param name="e">事件參數</param>
+        private void checkButton_Click(object sender, EventArgs e)
+        {
+            // TODO: 在此加入貸款資格審查的邏輯
+            const decimal MINIMUM_SALARY = 1200000m; // 最低年薪要求
+            const int MINIMUM_YEARS = 2; // 最低年資要求
+            decimal salary; // 使用者輸入的年薪
+            int yearsOnJob; // 使用者輸入的年資
+
+            try 
+            {
+                salary = decimal.Parse(salaryTextBox.Text); // 解析年薪轉化為 decimal
+                yearsOnJob = int.Parse(yearsTextBox.Text); // 解析年資轉化為 int
+
+                // 檢查使用者輸入的年薪及年資是否符合資格
+                if (salary >= MINIMUM_SALARY) 
+                {
+                    if(yearsOnJob>=MINIMUM_YEARS)
+                    {
+                        decisionLabel.Text = "符合資格";
+                    } else
+                    {
+                        decisionLabel.Text = "不符合資格：年資未達最低標準";
+                    }
+                        
+                } 
+                else 
+                {
+                    decisionLabel.Text = "不符合資格：收入未達最低標準";
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message); // 顯示錯誤訊息
+            }
+
+        }
+
+        /// <summary>
+        /// 當「清除」按鈕被點擊時執行的事件處理函式
+        /// 清空年薪、年資輸入欄位及審查結果標籤，並將游標移至年薪欄位
+        /// </summary>
+        /// <param name="sender">事件來源物件</param>
+        /// <param name="e">事件參數</param>
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            // 清空年薪輸入欄位
+            salaryTextBox.Text = "";
+            // 清空年資輸入欄位
+            yearsTextBox.Text = "";
+            // 清空審查結果標籤
+            decisionLabel.Text = "";
+
+            // 將游標移至年薪輸入欄位，方便使用者再次輸入
+            salaryTextBox.Focus();
+        }
+
+        /// <summary>
+        /// 當「離開」按鈕被點擊時執行的事件處理函式
+        /// 關閉表單，結束程式
+        /// </summary>
+        /// <param name="sender">事件來源物件</param>
+        /// <param name="e">事件參數</param>
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // 關閉表單
+            this.Close();
+        }
+    }
+}
