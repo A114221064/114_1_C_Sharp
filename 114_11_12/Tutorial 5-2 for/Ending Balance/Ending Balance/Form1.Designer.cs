69f0f4bd7488407f158a9bf19e14f7fe453bf4de
A114221064 <A114221064@mail.shu.edu.tw>
2025-11-12 12:54:09 +0800

    114_11_12

---
+++ 114_11_12/Tutorial 5-2 for/Ending Balance/Ending Balance/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Ending_Balance
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// 初始化元件的方法（由設計工具產生）。
+        /// 此處新增的註解皆以繁體中文撰寫，並將各控制項字型設定為 18pt。
+        /// 注意：僅變更 Text 與 Font 屬性及加入註解，其他屬性維持原樣以避免影響版面與邏輯。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startingBalancePromptLabel = new System.Windows.Forms.Label();
+            this.monthsPromptLabel = new System.Windows.Forms.Label();
+            this.startingBalTextBox = new System.Windows.Forms.TextBox();
+            this.monthsTextBox = new System.Windows.Forms.TextBox();
+            this.outputDescriptionLabel = new System.Windows.Forms.Label();
+            this.endingBalanceLabel = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.detailListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // startingBalancePromptLabel
+            // 
+            this.startingBalancePromptLabel.AutoSize = true;
+            this.startingBalancePromptLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startingBalancePromptLabel.Location = new System.Drawing.Point(121, 15);
+            this.startingBalancePromptLabel.Name = "startingBalancePromptLabel";
+            this.startingBalancePromptLabel.Size = new System.Drawing.Size(197, 40);
+            this.startingBalancePromptLabel.TabIndex = 0;
+            this.startingBalancePromptLabel.Text = "起始餘額：";
+            // 
+            // monthsPromptLabel
+            // 
+            this.monthsPromptLabel.AutoSize = true;
+            this.monthsPromptLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.monthsPromptLabel.Location = new System.Drawing.Point(121, 91);
+            this.monthsPromptLabel.Name = "monthsPromptLabel";
+            this.monthsPromptLabel.Size = new System.Drawing.Size(197, 40);
+            this.monthsPromptLabel.TabIndex = 2;
+            this.monthsPromptLabel.Text = "月份數量：";
+            // 
+            // startingBalTextBox
+            // 
+            this.startingBalTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startingBalTextBox.Location = new System.Drawing.Point(324, 12);
+            this.startingBalTextBox.Name = "startingBalTextBox";
+            this.startingBalTextBox.Size = new System.Drawing.Size(441, 48);
+            this.startingBalTextBox.TabIndex = 3;
+            // 
+            // monthsTextBox
+            // 
+            this.monthsTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.monthsTextBox.Location = new System.Drawing.Point(324, 83);
+            this.monthsTextBox.Name = "monthsTextBox";
+            this.monthsTextBox.Size = new System.Drawing.Size(441, 48);
+            this.monthsTextBox.TabIndex = 5;
+            // 
+            // outputDescriptionLabel
+            // 
+            this.outputDescriptionLabel.AutoSize = true;
+            this.outputDescriptionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputDescriptionLabel.Location = new System.Drawing.Point(76, 490);
+            this.outputDescriptionLabel.Name = "outputDescriptionLabel";
+            this.outputDescriptionLabel.Size = new System.Drawing.Size(125, 40);
+            this.outputDescriptionLabel.TabIndex = 6;
+            this.outputDescriptionLabel.Text = "結餘：";
+            // 
+            // endingBalanceLabel
+            // 
+            this.endingBalanceLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.endingBalanceLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.endingBalanceLabel.Location = new System.Drawing.Point(212, 479);
+            this.endingBalanceLabel.Name = "endingBalanceLabel";
+            this.endingBalanceLabel.Size = new System.Drawing.Size(466, 51);
+            this.endingBalanceLabel.TabIndex = 7;
+            this.endingBalanceLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateButton.Location = new System.Drawing.Point(66, 574);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(131, 55);
+            this.calculateButton.TabIndex = 8;
+            this.calculateButton.Text = "計算";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearButton.Location = new System.Drawing.Point(376, 574);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(135, 55);
+            this.clearButton.TabIndex = 9;
+            this.clearButton.Text = "清除";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(717, 574);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(130, 55);
+            this.exitButton.TabIndex = 10;
+            this.exitButton.Text = "離開";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // detailListBox
+            // 
+            this.detailListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.detailListBox.FormattingEnabled = true;
+            this.detailListBox.ItemHeight = 40;
+            this.detailListBox.Location = new System.Drawing.Point(111, 192);
+            this.detailListBox.Name = "detailListBox";
+            this.detailListBox.Size = new System.Drawing.Size(736, 244);
+            this.detailListBox.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(21F, 40F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(943, 765);
+            this.Controls.Add(this.detailListBox);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.endingBalanceLabel);
+            this.Controls.Add(this.outputDescriptionLabel);
+            this.Controls.Add(this.monthsTextBox);
+            this.Controls.Add(this.startingBalTextBox);
+            this.Controls.Add(this.monthsPromptLabel);
+            this.Controls.Add(this.startingBalancePromptLabel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "結餘計算";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label startingBalancePromptLabel;
+        private System.Windows.Forms.Label monthsPromptLabel;
+        private System.Windows.Forms.TextBox startingBalTextBox;
+        private System.Windows.Forms.TextBox monthsTextBox;
+        private System.Windows.Forms.Label outputDescriptionLabel;
+        private System.Windows.Forms.Label endingBalanceLabel;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.ListBox detailListBox;
+    }
+}
